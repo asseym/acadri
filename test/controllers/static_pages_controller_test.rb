11b8a468aa188360bf5ae2111d1b2efd6e15622e
Alfred Assey M <asseym@gmail.com>
2015-04-07 12:25:56 +0300

    finished users signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,30 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   
-  @base_title = "S!mple ERP"
+  def setup
+    @base_title = "S!mple ERP"
+  end
   
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", @base_title
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | " + @base_title
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | " + @base_title
+    assert_select "title", "About | #{@base_title}"
   end
   
   test "should get contact" do
       get :contact
       assert_response :success
-      assert_select "title", "Contact | " + @base_title
-    end
+      assert_select "title", "Contact | #{@base_title}"
+  end
 
 end
