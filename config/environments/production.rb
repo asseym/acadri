b61247f7fb7bf7d2c71623902a3eadd709fbb758
Assey Alfred M <asseym@gmail.com>
2016-04-10 15:04:39 +0300

    mailer configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => "localhost"}
+  Rails.application.routes.default_url_options[:host] = 'localhost'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
