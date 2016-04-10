a3d2e8d157157ff13589472a951c2b3bd0526f64
Assey Alfred M <asseym@gmail.com>
2016-04-10 14:35:22 +0300

    remove force ssl

--- config/deploy.rb
+++ config/deploy.rb
@@ -178,10 +178,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      within current_path do
-        # execute :rake, 'cache:clear'
-        Rails.cache.clear
-      end
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      #   # Rails.cache.clear
+      # end
       within current_path do
         invoke 'unicorn:restart'
       end
